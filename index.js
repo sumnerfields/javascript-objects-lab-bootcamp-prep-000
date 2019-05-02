7bbd4d7864b7c832017068e59922b505bb4e3cf2
Sumner Fields <sumnertfields@gmail.com>
2019-05-02 00:33:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
 }
 
 function deleteFromObjectByKey (object, key){
+   var newRecipes = Object.assign({}, object, { [key]: value })
 }
 
 function destructivelyDeleteFromObjectByKey (object, key) {
